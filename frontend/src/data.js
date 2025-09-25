7b43260ab4d4a2aa6fd8677b1399428f51238865
Luis Guilherme <lgduarte.santos2@hotmail.com>
2025-09-25 09:28:23 -0300

    Atualização

--- frontend/src/data.js
+++ frontend/src/data.js
@@ -1,9 +1,12 @@
 import logo from "./assets/logo.png";
 import brigadeiro from "./assets/brigadeiro.jpg"
+import brigadeiro_embalado from "./assets/brigadeiro_embalado.jpg"
 import doce_leite from "./assets/doce_de_leite.jpg"
-import prestigio from "./assets/prestigio.jpg"
+import doce_leite_embalado from "./assets/doce_de_leite_embalado.jpg"
 import doce_leite_coco from "./assets/doce_de_leite_coco.jpg"
-import brigadeiro2 from "./assets/brigadeiro2.jpg"
+import doce_leite_coco_embalado from "./assets/doce_leite_coco_embalado.jpg"
+import prestigio from "./assets/prestigio.jpg"
+import prestigio_embalado from "./assets/prestigio_embalado.jpg"
 
 
 export const product = [
@@ -12,27 +15,27 @@ export const product = [
         name: "Pão de Mel - Brigadeiro",
         description: "Massa deliciosa sabor brigadeiro",
         price: "R$ 8,00",
-        image: brigadeiro,
+        image: brigadeiro_embalado,
     },
     {
         id: 2,
         name: "Pão de Mel - Doce de leite",
         description: "Massa deliciosa sabor doce de leite",
         price: "R$ 7,00",
-        image: doce_leite,
+        image: doce_leite_embalado,
     },
     {
         id: 3,
         name: "Pão de Mel - Prestígio",
         description: "Massa deliciosa sabor prestígio",
         price: "R$ 8,00",
-        image: prestigio,
+        image: prestigio_embalado,
     },
     {
         id: 4,
         name: "Pão de Mel - Doce de leite com Coco",
         description: "Massa deliciosa sabor doce de leite com Coco",
         price: "R$ 8,00",
-        image: doce_leite_coco,
+        image: doce_leite_coco_embalado,
     }
 ];
